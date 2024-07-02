2f9d585f0d708ec81741bd42a5b86e10874b86fa
WhiteCilcle <ksq345@naver.com>
2024-07-02 16:02:21 +0900

    멥 디테일 추가
    행성 및 우주선 추가

---
+++ MiniGolfMap/Assets/Scripts/ObjectShooter.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class ObjectShooter : MonoBehaviour
+{
+    public float shootPower = 10f; // 발사 파워, 유니티 에디터에서 조절 가능
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody component missing from this game object");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        if (rb != null)
+        {
+            rb.AddForce(transform.forward * shootPower, ForceMode.Impulse); // 로컬 Z축으로 힘을 가합니다.
+        }
+    }
+}
