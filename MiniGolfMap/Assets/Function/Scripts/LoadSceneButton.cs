f666ff0162f1b43521224cc87e808718c3847503
Nabyeongho <127078321+nabh0203@users.noreply.github.com>
2024-07-04 18:54:06 +0900

    Title UI Effect

--- MiniGolfMap/Assets/Function/Scripts/LoadSceneButton.cs
+++ MiniGolfMap/Assets/Function/Scripts/LoadSceneButton.cs
@@ -7,20 +7,41 @@ using DG.Tweening;
 public class LoadSceneButton : MonoBehaviour
 {
     public string SceneName;
-    public CanvasGroup canvasGroup; // CanvasGroup 컴포넌트를 사용하여 Canvas의 투명도를 조절
+    //public CanvasGroup canvasGroup; // CanvasGroup 컴포넌트를 사용하여 Canvas의 투명도를 조절
+    //public GameObject canvas; // CanvasGroup 컴포넌트를 사용하여 Canvas의 투명도를 조절
     public DOTweenAnimation openDoorOne;
     public DOTweenAnimation openDoorTwo;
 
+    public Material hologram;
+
+    private void Start()
+    {
+        //hologram 안에 FADE_ON , HOLOGRAM_ON Setbool 이용해서 꺼주기
+        hologram.DisableKeyword("FADE_ON");
+        hologram.DisableKeyword("HOLOGRAM_ON");
+
+    }
     public void OnClickButton()
     {
         Debug.Log("버튼 눌림");
+        // 2초 후에 글리치 효과를 비활성화
+        StartCoroutine(GlitchEffect());
+    }
 
-        // Canvas를 서서히 사라지게 하는 애니메이션
-        canvasGroup.DOFade(0, 1f).OnComplete(() =>
-        {
-            // Canvas가 사라진 후 문 애니메이션 실행
-            OpenDoors();
-        });
+    private IEnumerator GlitchEffect()
+    {
+        hologram.EnableKeyword("FADE_ON");
+        hologram.EnableKeyword("HOLOGRAM_ON");
+        //hologram 안에 FADE_ON , HOLOGRAM_ON Setbool 이용해서 켜주기
+        // 홀로그램 효과 활성화
+        hologram.DOFloat(22f,"_StripesAmount", 3f); 
+        hologram.DOFloat(22f,"_UnChangedAmount", 3f);
+        hologram.DOFloat(1f, "_StripesSpeed", 3f); 
+        hologram.DOFloat(1f, "_FadeAmount", 3f); 
+        hologram.DOFloat(1f, "_BurnTransition", 3f);
+        //canvas.SetActive(false);
+        yield return new WaitForSeconds(4f);
+        OpenDoors();
     }
 
     private void OpenDoors()
@@ -28,8 +49,9 @@ public class LoadSceneButton : MonoBehaviour
         // 원하는 애니메이션 효과를 설정 (여기서는 문이 90도 회전하는 예시)
         openDoorOne.DOPlay();
         openDoorTwo.DOPlay();
+
         // 애니메이션이 끝난 후 씬 로드
-        DOVirtual.DelayedCall(1f, () => SceneManager.LoadScene(SceneName));
-        VR_Fade_NBH.instance.FadeOut();
+        DOVirtual.DelayedCall(6f, () => SceneManager.LoadScene(SceneName));
+        DOVirtual.DelayedCall(6f, () => DOTween.KillAll());
     }
 }
